e20bdd20f28998328a8d9dc4df4aaf00b4a931e7
Sudhansu Singh <sudhansu@ith.tech>
2023-08-25 00:36:44 +0530

    ok

--- server.js
+++ server.js
@@ -29,15 +29,50 @@ app.get("/download", async (req,res)=>{
 
     let urlToSend = [];
     urlToSend = data.info.filter((el)=>{ 
-        if(el.hasVideo && el.hasVideo && el.container == "mp4"){
+        if(el.hasAudio && el.container == "mp4"){
+            // console.log(el)
             return el.url;
         }
      });
+
+    data.info = urlToSend;
     urlToSend = urlToSend.map(e => e.url)
     return res.render("./download", data);
 })
 
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 30000;
 app.listen(PORT, () => {
     console.log(`🚒 Server is running on port ${PORT} 🤠🛑`);
+})
+
+
+
+
+const back = require('androidjs').back;
+
+back.on('get-data', async function(url){
+
+    
+    const videoId = url.split("v=")[1];
+    const info = await youtubeDownloader.getInfo(url);
+
+    let data ={
+		url: "https://www.youtube.com/embed/" + videoId,
+        info: info.formats.sort((a, b) => {
+            return a.mimeType < b.mimeType;
+        })
+    };
+
+    let urlToSend = [];
+    urlToSend = data.info.filter((el)=>{ 
+        if(el.hasAudio && el.container == "mp4"){
+            // console.log(el)
+            return el.url;
+        }
+     });
+
+    data.info = urlToSend;
+    urlToSend = urlToSend.map(e => e.url);
+    back.send('get-data-result', data);
+    // return res.render("./download", data);
 })
