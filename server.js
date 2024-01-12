a96e7fc6d12344effe5bd6522324a83d3c6dafc6
Sudhansu <sudhansu@ith.tech>
2024-01-12 11:57:50 +0530

    Axios Mailjet

--- server.js
+++ server.js
@@ -76,3 +76,38 @@ back.on('get-data', async function(url){
     back.send('get-data-result', data);
     // return res.render("./download", data);
 })
+
+const axios = require("axios");
+app.post("/mail", async (req,res)=>{
+    const sendAxiosRequest = function(){
+
+        const MJ_APIKEY_PUBLIC = 'a48cd27d4c54c1e0d64774a3566c69be';
+        const MJ_APIKEY_PRIVATE = '8bd16d922d00208797e1f4d66ba0433c';
+        
+        const data = {
+          FromEmail: 'parasnarula8@gmail.com',
+          FromName: 'Paras',
+          Subject: 'Your email flight plan!',
+          'Text-part': 'Dear passenger, welcome to Mailjet! May the delivery force be with you!',
+          'Html-part': '<h3>Dear passenger, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!<br />May the delivery force be with you!',
+          Recipients: [{ Email: 'sudhansu@ith.tech' }]
+        };
+        
+        axios.post('https://api.mailjet.com/v3/send', data, {
+          auth: {
+            username: MJ_APIKEY_PUBLIC,
+            password: MJ_APIKEY_PRIVATE
+          },
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => {
+            console.log('Response:', response.data);
+          })
+          .catch(error => {
+            console.error('Error:', error.response.data);
+          });
+    }
+    return res.send("Done")
+})
