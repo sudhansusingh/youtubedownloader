f0ab56a30dd6d7e2143e78eb51573cc3c3230cf7
Sudhansu <sudhansu@ith.tech>
2024-01-12 12:07:33 +0530

    Email fix

--- server.js
+++ server.js
@@ -10,30 +10,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+var cors = require('cors')
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
+
 app.get("/", (req, res) => {
     res.render("index");
 });
 const youtubeDownloader = require("ytdl-core");
 
-app.get("/download", async (req,res)=>{
+app.get("/download", async (req, res) => {
 
     const videoId = req.query.url.split('v=')[1];
     const info = await youtubeDownloader.getInfo(req.query.url);
 
-    let data ={
-		url: "https://www.youtube.com/embed/" + videoId,
+    let data = {
+        url: "https://www.youtube.com/embed/" + videoId,
         info: info.formats.sort((a, b) => {
             return a.mimeType < b.mimeType;
         })
     };
 
     let urlToSend = [];
-    urlToSend = data.info.filter((el)=>{ 
-        if(el.hasAudio && el.container == "mp4"){
+    urlToSend = data.info.filter((el) => {
+        if (el.hasAudio && el.container == "mp4") {
             // console.log(el)
             return el.url;
         }
-     });
+    });
 
     data.info = urlToSend;
     urlToSend = urlToSend.map(e => e.url)
@@ -50,26 +62,26 @@ app.listen(PORT, () => {
 
 const back = require('androidjs').back;
 
-back.on('get-data', async function(url){
+back.on('get-data', async function (url) {
 
-    
+
     const videoId = url.split("v=")[1];
     const info = await youtubeDownloader.getInfo(url);
 
-    let data ={
-		url: "https://www.youtube.com/embed/" + videoId,
+    let data = {
+        url: "https://www.youtube.com/embed/" + videoId,
         info: info.formats.sort((a, b) => {
             return a.mimeType < b.mimeType;
         })
     };
 
     let urlToSend = [];
-    urlToSend = data.info.filter((el)=>{ 
-        if(el.hasAudio && el.container == "mp4"){
+    urlToSend = data.info.filter((el) => {
+        if (el.hasAudio && el.container == "mp4") {
             // console.log(el)
             return el.url;
         }
-     });
+    });
 
     data.info = urlToSend;
     urlToSend = urlToSend.map(e => e.url);
@@ -78,38 +90,38 @@ back.on('get-data', async function(url){
 })
 
 const axios = require("axios");
-app.post("/mail", async (req,res)=>{
+app.post("/mail", cors(corsOptions), async (req, res) => {
     // const sendAxiosRequest = function(){
 
-        const MJ_APIKEY_PUBLIC = 'a48cd27d4c54c1e0d64774a3566c69be';
-        const MJ_APIKEY_PRIVATE = '8bd16d922d00208797e1f4d66ba0433c';
-        
-        const data = {
-          FromEmail: 'parasnarula8@gmail.com',
-          FromName: 'Paras',
-          Subject: 'Your email flight plan!',
-          'Text-part': 'Dear passenger, welcome to Mailjet! May the delivery force be with you!',
-          'Html-part': '<h3>Dear passenger, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!<br />May the delivery force be with you!',
-          Recipients: [{ Email: 'sudhansu@ith.tech' }]
-        };
-        
-        axios.post('https://api.mailjet.com/v3/send', data, {
-          auth: {
+    const MJ_APIKEY_PUBLIC = 'a48cd27d4c54c1e0d64774a3566c69be';
+    const MJ_APIKEY_PRIVATE = '8bd16d922d00208797e1f4d66ba0433c';
+
+    const data = {
+        FromEmail: 'parasnarula8@gmail.com',
+        FromName: 'Paras',
+        Subject: 'Your email flight plan!',
+        'Text-part': 'Dear passenger, welcome to Mailjet! May the delivery force be with you!',
+        'Html-part': '<h3>Dear passenger, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!<br />May the delivery force be with you!',
+        Recipients: [{ Email: 'sudhansu@ith.tech' }]
+    };
+
+    axios.post('https://api.mailjet.com/v3/send', data, {
+        auth: {
             username: MJ_APIKEY_PUBLIC,
             password: MJ_APIKEY_PRIVATE
-          },
-          headers: {
+        },
+        headers: {
             'Content-Type': 'application/json'
-          }
-        })
-          .then(response => {
+        }
+    })
+        .then(response => {
             console.log('Response:', response.data);
             return res.send(response.data)
-          })
-          .catch(error => {
+        })
+        .catch(error => {
             console.error('Error:', error.response.data);
             return res.send(error.response.data)
 
-          });
+        });
     // }
 })
